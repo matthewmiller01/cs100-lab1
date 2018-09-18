fe7192427a5049fd0b2666cc8f4d3bfeb4a94e4e
Matthew Miller <matthewmiller@Matthews-MacBook-Pro.local>
2018-09-18 13:05:27 -0400

    final submission mm

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const API_KEY = "6a6275c0-b77e-11e8-a4d1-69890776a30b";
 
+//shows all the galleries if the page is loaded
 document.addEventListener("DOMContentLoaded", () => {
   const url = `https://api.harvardartmuseums.org/gallery?apikey=${API_KEY}`;
   showGalleries(url);
@@ -8,37 +9,74 @@ document.addEventListener("DOMContentLoaded", () => {
 function showGalleries(url) {
   fetch(url)
   .then(response => response.json())
+  //callbacks so that once it has the data it can display each gallery
   .then(data => {
     data.records.forEach(gallery => {
+      //first sets a warning that a gallery has no objects so person doesn't click there (extra feature)
+      if (gallery.objectcount == 0)
+      {
+        document.querySelector("#galleries").innerHTML += `
+          Objectless
+        `
+      }
+      //adds information on each gallery, including an object count
       document.querySelector("#galleries").innerHTML += `
         <li>
           <a href="#${gallery.id}" onclick="showObjectsTable(${gallery.id})">
-            Gallery #${gallery.id}: ${gallery.name} (Floor ${gallery.floor})
+            Gallery #${gallery.id}: ${gallery.name} (Floor ${gallery.floor}) Object Count: ${gallery.objectcount}
           <p></p>
           </a>
-        </li>
+        </li><li></li>
       `;
     });
     if (data.info.next) {
       showGalleries(data.info.next);
     }
   })
+  //doesn't display other HTML elements on this page
+  document.querySelector("#all-objects").style.display = "none";
+  document.querySelector("#boton").style.display = "none";
+  document.querySelector("#single-object").style.display = "none";
 }
 
+//back button but only on an individual object page since was funky for a single gallery
+window.addEventListener("hashchange", () => {
+  // the galleries have id lengths of four numbers
+  //so checking the length of the previous page's hash can tell you if you have to go back to that gallery
+  //this was a trick that Mohamed Ally helped me think through earlier today
+  var hash = window.location.hash.slice(1);
+  if(hash.length == 4)
+  {
+    showObjectsTable(hash);
+  }
+//  else {
+//      window.location.reload();
+//  }
+});
+
+//shows table of objects in a specific gallery
 function showObjectsTable(id) {
   const ids = `https://api.harvardartmuseums.org/object?gallery=${id}&apikey=${API_KEY}`;
   console.log(ids);
   fetch(ids)
   .then(response => response.json())
   .then(data => {
+    // this is an added feature of pointing out when there are no items in the collection for user
+    if (data.records.objectcount == null)
+    {
+      console.log(data);
+      document.querySelector("#objects").innerHTML = `
+        Lamentably, there are no objects in this collection.
+        <li> Reload the page to return to the gallery list</li>
+      `
+    }
+    //creates a list of people who contributed to the object to list as contributors
     data.records.forEach(object => {
-      console.log(object);
       var names = "";
       if (object.people != null)
       {
         object.people.forEach(people =>{
           names+= people.name + " ";
-          console.log(names);
         })
       }
        document.querySelector("#objects").innerHTML += `
@@ -50,12 +88,19 @@ function showObjectsTable(id) {
           </li>
           <li>
             <a href = "${object.url}">URL to Main Page</a> </li> `
-
+        //checks if object has an image and displays it if it does
         if(object.primaryimageurl)
         {
             document.querySelector("#objects").innerHTML += `
             <img id = "newImage" src = "${object.primaryimageurl}"></img>)
         `;
+      }
+      else {
+        {
+          document.querySelector("#objects").innerHTML+= `
+            No image available.
+          `;
+        }
       }
     document.querySelector("#objects").innerHTML += `</a>`
     });
@@ -64,7 +109,7 @@ function showObjectsTable(id) {
   document.querySelector("#all-galleries").style.display = "none";
 }
 
-
+//shows description of a single object
 function showObjectDescription(idg) {
   console.log(idg);
   const objurl = `https://api.harvardartmuseums.org/object/${idg}?apikey=${API_KEY}`;
@@ -72,11 +117,37 @@ function showObjectDescription(idg) {
   fetch(objurl)
   .then(response => response.json())
   .then(data => {
+    //many conditions so that if object missing an attribute, it displays none instead
     document.querySelector("#singleobject").innerHTML += `
-      <li> Work Title: ${data.title} </li>
-      <li> Description: ${data.description} </li>
-      <li>Provenance: ${data.provenance}</li>
-      <li> Accession Year: ${data.accessionyear}</li>`
+      <li> Work Title: ${data.title} </li>`
+    var description = "None";
+    if(data.description)
+    {
+      description = data.description;
+    }
+    document.querySelector("#singleobject").innerHTML += `
+      <li> Description: ${description} </li>`
+    var prov = "None";
+    if(data.provenance)
+    {
+      prov = data.provenance;
+    }
+    document.querySelector("#singleobject").innerHTML += `
+      <li>Provenance: ${prov}</li>`
+    var accessionyear = "Unknown";
+    if(data.accessionyear)
+    {
+      accessionyear = data.accessionyear;
+    }
+    document.querySelector("#singleobject").innerHTML += `
+      <li> Accession Year: ${accessionyear}</li>`
+    if(data.primaryimageurl)
+      {
+          document.querySelector("#singleobject").innerHTML += `
+          <img id = "newImage" src = "${data.primaryimageurl}"></img>)
+      `;
+    }
+    //does not display other HTML elements here
   document.querySelector("#all-objects").style.display = "none";
   document.querySelector("#all-galleries").style.display = "none";
 })
