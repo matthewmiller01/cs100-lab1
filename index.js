2ce30e50fb5b0f675df8f435b1dc71745207be5d
Matthew Miller <matthewmiller@Matthews-MacBook-Pro.local>
2018-09-18 02:04:51 -0400

    gallery and object view

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const API_KEY = "API_KEY_HERE";
+const API_KEY = "6a6275c0-b77e-11e8-a4d1-69890776a30b";
 
 document.addEventListener("DOMContentLoaded", () => {
   const url = `https://api.harvardartmuseums.org/gallery?apikey=${API_KEY}`;
@@ -14,6 +14,7 @@ function showGalleries(url) {
         <li>
           <a href="#${gallery.id}" onclick="showObjectsTable(${gallery.id})">
             Gallery #${gallery.id}: ${gallery.name} (Floor ${gallery.floor})
+          <p></p>
           </a>
         </li>
       `;
@@ -25,6 +26,57 @@ function showGalleries(url) {
 }
 
 function showObjectsTable(id) {
+  const ids = `https://api.harvardartmuseums.org/object?gallery=${id}&apikey=${API_KEY}`;
+  console.log(ids);
+  fetch(ids)
+  .then(response => response.json())
+  .then(data => {
+    document.querySelector('#suh').innerHTML += `
+    <a href="#suh" onclick="showGalleries(url)"></a>
+    `
+    data.records.forEach(object => {
+      console.log(object);
+      var names = "";
+      if (object.people != null)
+      {
+        object.people.forEach(people =>{
+          names+= people.name + " ";
+          console.log(names);
+        })
+      }
+      document.querySelector("#objects").innerHTML += `
+        <li>
+          <a href="#${object.exact_title}" onclick="showObjectDescription(${object.objectid})">
+            Work Title: ${object.title}
+          </li>
+          <li> Author(s): ${names}
+          </li>
+          <li>
+            <a href = "${object.url}">URL to Main Page</a> </li> `
+
+        if(object.primaryimageurl)
+        {
+            document.querySelector("#objects").innerHTML += `
+            <img id = "newImage" src = "${object.primaryimageurl}"></img>)
+        `;
+      }
+    document.querySelector("#objects").innerHTML += `</a>`
+    });
+})
+  document.querySelector("#all-objects").style.display = "block";
+  document.querySelector("#all-galleries").style.display = "none";
+}
+
+function showObjectDescription(idg) {
+  const objurl = `https://api.harvardartmuseums.org/object/${idg}&apikey=${API_KEY}`;
+  console.log(objurl);
+  fetch(objurl)
+  .then(response => response.json())
+  .then(data => {
+      document.querySelector("#singleobject").innerHTML += `
+        Work Title: ${object.title}`
+      console.log("#singleobject");
+  });
   document.querySelector("#all-objects").style.display = "block";
   document.querySelector("#all-galleries").style.display = "none";
 }
